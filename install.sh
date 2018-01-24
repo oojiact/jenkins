39366d2d9e01a9024d5a97a7c3ed75e9a25a1413
oojiact <33948432+oojiact@users.noreply.github.com>
2018-01-24 18:57:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt-get -y update
-sudo apt-get -y install jenkins
+sudo apt-get update
+sudo apt-get install jenkins
 sudo systemctl start jenkins
 sudo ufw allow 8080
 sudo ufw enable
 sudo ufw allow OpenSSH
-apt-gat -y install haveged
+apt-gat install haveged
